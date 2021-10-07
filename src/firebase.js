383cf9e69a3b52156a4310690e31c6b24f22a1c2
Raja <rajasingh8889@gmail.com>
2021-10-07 17:54:08 +0530

    second changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCDvpALb9u1Qn3IXWSNSjP0NtejbWNzsUY",
-  authDomain: "chatdom-54ec5.firebaseapp.com",
-  projectId: "chatdom-54ec5",
-  storageBucket: "chatdom-54ec5.appspot.com",
-  messagingSenderId: "1050566466990",
-  appId: "1:1050566466990:web:d25b78788d10b75656fc3f",
-  measurementId: "G-VHJTWNKJ5Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MES_ID,
 });
 
 const db = firebaseApp.firestore();
@@ -15,5 +15,5 @@ const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
 
-export { auth,provider, storage}
+export { auth, provider, storage };
 export default db;
